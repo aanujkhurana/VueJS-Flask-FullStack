39aad0791333355cc652b9228c47094c7b5511cc
aanujkhurana <aanujkhurana@gmail.com>
2024-04-21 00:28:08 +1000

    Backend-Flask-Setup⏳

--- Backend/main.py
+++ Backend/main.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 CORS(app, resources={r"/*": {'origins': '*'}})
+
+
+# HELLO WORLD ROUTE
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({'message': 'Hello, World!'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
